4668ad7af569f16818f14deef467c0cdef81c215
Arthur TOledo <arthurtoledo@pop-os.localdomain>
2021-03-07 20:38:20 -0300

    File transformation in linux

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-export const Greeter = (name: string) => `Hello ${name}`;
+import * as path from 'path';
+import * as child_process from 'child_process';
+
+export const convert = async (pathFile: string, extOutput: string, outputDir: string): Promise<string> => {
+  const system = process.platform;
+  const extension = path.extname(pathFile);
+  const fileName = path.basename(pathFile, extension);
+  const fullName = path.basename(pathFile);
+  const convertCommandLinux = `${path.resolve(__dirname, 'instdir', 'program', 'soffice.bin')} --headless --norestore --invisible --nodefault --nofirststartwizard --nolockcheck --nologo --convert-to ${extOutput} --outdir ${outputDir} ${pathFile}`;
+
+  if (!fullName.match(/\.(doc|docx|pdf|odt)$/)) {
+    throw new Error('Invalid file format, see the documentation for more information.');
+  } else if (!extOutput.match(/(doc|docx|pdf|odt)$/)) {
+    throw new Error('Format to be converted not accepted');
+  }
+
+  try {
+    if (system === 'linux') { child_process.execSync(convertCommandLinux).toString('utf8') }
+    if (system === 'win32') { /*  */ }
+  } catch (e) {
+    throw new Error('Error converting the file');
+  }
+
+  return path.join(outputDir, `${fileName}.${extOutput}`);
+}
---
+++ src/utils/download-libreoffice.ts
@@ -0,0 +1,42 @@
+import { createWriteStream, unlinkSync } from 'fs';
+import * as util from 'util';
+import * as stream from 'stream';
+import * as tar from 'tar';
+import * as path from 'path';
+import axios from 'axios';
+
+const finished = util.promisify(stream.finished);
+const libreOfficeDownloadPath = createWriteStream('./src/lo.tar.gz');
+
+const identifySystemOS = async (): Promise<string> => {
+  return process.platform;
+}
+
+const downloadLibreOffice = async (typeSystemOS: string) => {
+  if (typeSystemOS === 'win32') {
+    /* WINDOWS PROCESS */
+  }
+
+  if (typeSystemOS === 'linux') {
+    writeArchiveDownload().catch(console.error);
+  }
+}
+
+async function writeArchiveDownload() {
+  const response = await axios.get('https://github.com/vladgolubev/serverless-libreoffice/releases/download/v6.1.0.0.alpha0/lo.tar.gz', {responseType: "stream"});
+  await finished(response.data.pipe(libreOfficeDownloadPath));
+  await tar.extract(
+    {
+      file: path.join('./src/lo.tar.gz'),
+      C: path.join('./src/')
+    }
+  )
+  unlinkSync(path.resolve(__dirname, 'lo.tar.gz'))
+}
+
+async function run() {
+  const system = await identifySystemOS();
+  await downloadLibreOffice(system);
+}
+
+run();
